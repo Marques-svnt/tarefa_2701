beb2e8ecfc79de67c74f6236b0065e6b8babc2a4
marques-svnt <marques.svnt.002@gmail.com>
2025-01-29 23:48:21 -0300

    Implementação da lógica da animação

--- main.c
+++ main.c
@@ -17,39 +17,43 @@ static volatile uint a = 1;
 static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
 
 // Variável global para armazenar a cor (Entre 0 e 255 para intensidade)
-uint8_t led_r = 0; // Intensidade do vermelho
-uint8_t led_g = 0; // Intensidade do verde
+uint8_t led_r = 0;   // Intensidade do vermelho
+uint8_t led_g = 0;   // Intensidade do verde
 uint8_t led_b = 100; // Intensidade do azul
 
 int main()
 {
-    //Inicializações
+    // Inicializações
     pinosInit();
     stdio_init_all();
     initializePio();
 
-    bool ultimo_estado_botao = true;      // Último estado do botão (inicialmente solto)
+    bool ultimo_estado_botao = true; // Último estado do botão (inicialmente solto)
     int index = 0;
     // Loop Principal
     while (1)
     {
-        //ledRed(); // Função responsável por fazer o led piscar 5 vezes por segundo
+        ledRed(); // Função responsável por fazer o led piscar 5 vezes por segundo
 
         bool estado_atual_botao_A = gpio_get(button_A);
         bool estado_atual_botao_B = gpio_get(button_B);
 
         // Condições associadas aos botões
-        if (estado_atual_botao_A == false && ultimo_estado_botao == true) {
-            if(index != 9){
+        if (estado_atual_botao_A == false && ultimo_estado_botao == true)
+        {
+            if (index != 9)
+            {
                 index++;
                 printf("A = %d\n", index);
-                ledRed();
+                set_one_led(index, led_r, led_g, led_b);
             }
-        } else if (estado_atual_botao_B == false && ultimo_estado_botao == true)
-            if(index != 0){
+        }
+        else if (estado_atual_botao_B == false && ultimo_estado_botao == true)
+            if (index != 0)
+            {
                 index--;
                 printf("A = %d\n", index);
-                ledRed();
+                set_one_led(index, led_r, led_g, led_b);
             }
         ultimo_estado_botao = estado_atual_botao_A;
         ultimo_estado_botao = estado_atual_botao_B;
--- src/pio.c
+++ src/pio.c
@@ -8,15 +8,69 @@
 #define IS_RGBW false
 #define NUM_PIXELS 25
 #define WS2812_PIN 7
-#define tempo 400
+
 
 // Buffer para armazenar quais LEDs estão ligados matriz 5x5
-bool led_buffer[NUM_PIXELS] = {
-    0, 0, 1, 1, 0, 
+bool led_buffer[10][NUM_PIXELS] = {
+    {0, 1, 1, 1, 0, 
+    0, 1, 0, 1, 0, 
+    0, 1, 0, 1, 0, 
+    0, 1, 0, 1, 0, 
+    0, 1, 1, 1, 0,},
+
+    {0, 0, 1, 0, 0, 
+    0, 0, 1, 0, 0, 
+    0, 0, 1, 0, 0, 
+    0, 0, 1, 0, 0, 
+    0, 0, 1, 0, 0,},
+    
+    {0, 1, 1, 1, 0, 
+    0, 0, 0, 1, 0, 
+    0, 1, 1, 1, 0, 
+    0, 1, 0, 0, 0, 
+    0, 1, 1, 1, 0,},
+    
+    {0, 1, 1, 1, 0, 
+    0, 0, 0, 1, 0, 
+    0, 1, 1, 1, 0, 
+    0, 0, 0, 1, 0, 
+    0, 1, 1, 1, 0,},
+    
+    {0, 1, 0, 1, 0, 
+    0, 1, 0, 1, 0, 
+    0, 1, 1, 1, 0, 
+    0, 0, 0, 1, 0, 
+    0, 0, 0, 1, 0,},
+    
+    {0, 1, 1, 1, 0, 
     0, 1, 0, 0, 0, 
     0, 1, 1, 1, 0, 
     0, 0, 0, 1, 0, 
-    0, 1, 1, 1, 0
+    0, 1, 1, 1, 0,},
+    
+    {0, 1, 1, 1, 0, 
+    0, 1, 0, 0, 0, 
+    0, 1, 1, 1, 0, 
+    0, 1, 0, 1, 0, 
+    0, 1, 1, 1, 0,},
+    
+    {0, 1, 1, 1, 0, 
+    0, 0, 0, 1, 0, 
+    0, 0, 0, 1, 0, 
+    0, 0, 0, 1, 0, 
+    0, 0, 0, 1, 0,},
+    
+    {0, 1, 1, 1, 0, 
+    0, 1, 0, 1, 0, 
+    0, 1, 1, 1, 0, 
+    0, 1, 0, 1, 0, 
+    0, 1, 1, 1, 0,},
+    
+    {0, 1, 1, 1, 0, 
+    0, 1, 0, 1, 0, 
+    0, 1, 1, 1, 0, 
+    0, 0, 0, 1, 0, 
+    0, 0, 0, 1, 0,},
 };
 
 static inline void put_pixel(uint32_t pixel_grb)
@@ -30,7 +84,7 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
 }
 
 
-void set_one_led(uint8_t r, uint8_t g, uint8_t b)
+void set_one_led(int index, int8_t r, uint8_t g, uint8_t b)
 {
     // Define a cor com base nos parâmetros fornecidos
     uint32_t color = urgb_u32(r, g, b);
@@ -38,7 +92,7 @@ void set_one_led(uint8_t r, uint8_t g, uint8_t b)
     // Define todos os LEDs com a cor especificada
     for (int i = 0; i < NUM_PIXELS; i++)
     {
-        if (led_buffer[i])
+        if (led_buffer[index][i])
         {
             put_pixel(color); // Liga o LED com um no buffer
         }
